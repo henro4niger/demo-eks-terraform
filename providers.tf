73e482562d85c4b65250d23acf4786d1169d14a8
henro4niger <52746166+henro4niger@users.noreply.github.com>
2023-06-20 13:57:45 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.65"
+    }
+  }
+}
+
+
+provider "helm" {
+  kubernetes {
+    config_path            = "~/.kube/config"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
